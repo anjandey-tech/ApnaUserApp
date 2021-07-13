c8e90febfb189120591d224bf6675b781285ab99
anjandey-tech <63156047+anjandey-tech@users.noreply.github.com>
2021-07-13 22:17:30 +0530

    21 bottom navigation

---
+++ app/src/main/java/com/inturnsala/apnauserapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.inturnsala.apnauserapp;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity
+{
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        navController = Navigation.findNavController(this,R.id.frame_layout);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
+
+    }
+}
---
+++ app/src/main/java/com/inturnsala/apnauserapp/ui/faculty/FavultyFragment.java
@@ -0,0 +1,25 @@
+package com.inturnsala.apnauserapp.ui.faculty;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.inturnsala.apnauserapp.R;
+
+public class FavultyFragment extends Fragment {
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favulty, container, false);
+    }
+}
